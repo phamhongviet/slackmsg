e538e9507883d491a04814dcbd32ffae3bf2d8ed
Viet Pham <phamhongviet52@gmail.com>
2016-04-28 11:56:38 +0700

    Initial Commit

---
+++ slackmsg.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python2.7
+"""
+slackmsg handles sending messages via slack
+"""
+
+import sys
+import getopt
+from urllib import urlencode
+import pycurl
+
+
+class SlackMsg(object):
+    """
+    SlackMsg handles sending messages via slack
+    """
+
+    def __init__(self, hook, channel="#random", user="slackbot", icon=":slack:"):
+        self.channel = channel
+        self.user = user
+        self.icon = icon
+        self.hook = hook
+
+    def send(self, text):
+        payload = '{"channel": "%s", "username": "%s", "icon_emoji": "%s", "text": "%s"}'\
+                  % (self.channel, self.user, self.icon, text)
+        postdata = urlencode({'payload': payload})
+        curl = pycurl.Curl()
+        curl.setopt(curl.POSTFIELDS, postdata)
+        curl.setopt(curl.URL, self.hook)
+        curl.perform()
+
+
+if __name__ == "__main__":
+    opts, _ = getopt.getopt(sys.argv[1:], 'c:u:i:t:h:')
+
+    for opt, arg in opts:
+        if opt == '-c':
+            flag_channel = arg
+        elif opt == '-u':
+            flag_user = arg
+        elif opt == '-i':
+            flag_icon = arg
+        elif opt == '-t':
+            flag_text = arg
+        elif opt == '-h':
+            flag_hook = arg
+        else:
+            assert False, "Unknown option: %s" % opt
+
+    try:
+        sm = SlackMsg(flag_hook)
+    except NameError:
+        exit(1)
+
+    try:
+        sm.channel = flag_channel
+    except NameError:
+        pass
+
+    try:
+        sm.user = flag_user
+    except NameError:
+        pass
+
+    try:
+        sm.icon = flag_icon
+    except NameError:
+        pass
+
+    try:
+        sm.send(flag_text)
+    except NameError:
+        exit(2)
